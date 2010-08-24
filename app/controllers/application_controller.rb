7e3dfd450416da9af922f72c572836ef4b2bc9dc
LuisVM <whizkas@hotmail.com>
2010-08-23 23:50:41 -0500

    Added authorization for site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,17 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   layout 'application'
+  before_filter :authenticate
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "zaznova" && password == "vawd"
+    end
+  end
 end
 
